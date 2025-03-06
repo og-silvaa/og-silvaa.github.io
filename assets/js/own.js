127469fa3fb55767502e73f112f3b6da912eb4d7
og-silvaa <ogsilvaa@gmail.com>
2025-03-06 15:00:22 -0500

    cambio de nombre, modificacion del borde de los proyectos y colocación de iconos, add tecnologias y mapa de iconos en data.json renderización de tecnologías en perfiles

--- assets/js/own.js
+++ assets/js/own.js
@@ -16,24 +16,42 @@ function updatePage() {
   proyectos(jsonData.proyectos);
 }
 
-function proyectos(proyectos){
-    let contenedor= document.getElementById("proyectos")
-    proyectos.forEach(proyecto=>{
-      if(proyecto.visible==false) return;
-        let card= `
+function proyectos(proyectos) {
+  let contenedor = document.getElementById("proyectos")
+  proyectos.forEach(proyecto => {
+    if (proyecto.visible == false) return;
+    let card = `
           <div class="proyecto">
           <h3>${proyecto.titulo}</h3>
           ${entidad(proyecto)}
           <div>${proyecto.descripcion}</div>
+          ${tecnologias(proyecto.tecnologias)}
         </div>`;
-        contenedor.innerHTML +=card;
-    });
+    contenedor.innerHTML += card;
+  });
 }
-function entidad(proyecto){
-  let card=``;
-  if(proyecto.entidadVisible || new Date().getFullYear()-proyecto.anio>4)
-    card+=`<h4>${proyecto.entidad} - ${proyecto.anio}</h4>`;
+function entidad(proyecto) {
+  let card = ``;
+  if (proyecto.entidadVisible || new Date().getFullYear() - proyecto.anio > 4)
+    card += `<h4>${proyecto.entidad} - ${proyecto.anio}</h4>`;
   else
-    card+=`<h4>Importante ${proyecto.rubro} en ${proyecto.pais} - ${proyecto.anio}</h4>`;
+    card += `<h4>Importante ${proyecto.rubro} en ${proyecto.pais} - ${proyecto.anio}</h4>`;
   return card;
+}
+function tecnologias(tecnologias) {
+  if(tecnologias==undefined) return "";
+  let card = `<div>`;
+
+  tecnologias.forEach(tecnologia => {
+    let icono = jsonData.iconos.find(icono => tecnologia == Object.keys(icono)[0])
+    console.log(tecnologia);
+    console.log(icono);
+    
+    if (icono!=undefined) {
+      card += `<img class="icono" src="images/icon/${Object.values(icono)[0]}.png" title="${tecnologia}">`;
+    } else {
+      card += `${tecnologia}   `
+    }
+  });
+  return card+"</div>";
 }
